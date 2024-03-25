5badffa627aa0ae56a7bee1a2f9092a28c08a3e1
Neil L <141323568+nlachan@users.noreply.github.com>
2024-03-25 01:32:48 -0700

    updated script

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { MovieCard } from "../Movie-card/movie-card";
+import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
