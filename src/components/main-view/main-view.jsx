f7c1eca5111a2f67d14d8c6ad38e922ad3331e83
Neil L <141323568+nlachan@users.noreply.github.com>
2024-03-26 16:44:06 -0700

    edited

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
