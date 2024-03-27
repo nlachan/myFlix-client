33f352c2b8ce2e20d35c5d119608d6c40200f685
Neil L <141323568+nlachan@users.noreply.github.com>
2024-03-26 19:19:37 -0700

    edited

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { useState } from "react";
-import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import "./login-view.scss";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
   const handleSubmit = (event) => {
     // this prevents the default behavior of the form which is to reload the entire page
     event.preventDefault();
 
+    // Assigning the username and password to the state
     const data = {
-      UserName: username,
-      Password: password,
+      username: username,
+      password: password,
     };
 
+    // lgging in the user
     fetch("https://naleen-movies-flix-8a1ae7a6e039.herokuapp.com/login", {
       method: "POST",
       headers: {
@@ -28,8 +30,8 @@ export const LoginView = ({ onLoggedIn }) => {
       .then((data) => {
         console.log("Login response: ", data);
         if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
           localStorage.setItem("token", data.token);
-          localStorage.setItem("user", data.user);
           onLoggedIn(data.user, data.token);
         } else {
           alert("No such user");
@@ -41,37 +43,28 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <h0> Login to your account </h0>
+    <Form className="loginForm" onSubmit={handleSubmit}>
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control
           type="text"
-          minLength={5}
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
         />
       </Form.Group>
-      <br />
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
         <Form.Control
           type="password"
-          minLength={8}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
       </Form.Group>
-      <br />
-      <Button type="submit" className="mt-2">
+      <Button variant="primary" type="submit">
         Login
       </Button>
     </Form>
   );
-};
-
-LoginView.propTypes = {
-  onLoggedIn: PropTypes.func.isRequired,
 };
